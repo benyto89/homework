1d21797050039f9cfc9a6a50051d5b0e5dd5537a
Benas Jank <benediktas.jankaitis@gmail.com>
2023-04-09 21:06:02 +0300

    Added form and validator packages + some tools for faster development (maker-bundle, profiler, test packs)

--- code/config/bundles.php
+++ code/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
